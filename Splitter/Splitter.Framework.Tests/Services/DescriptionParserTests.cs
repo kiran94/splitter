30e61a9ee0e5a8fca04a364c024ee5ab21d5c901
Kiran94 <kiran_patel94@hotmail.com>
2018-04-21 16:19:59 +0100

    Added test project and DescriptionParser tests

---
+++ Splitter/Splitter.Framework.Tests/Services/DescriptionParserTests.cs
@@ -0,0 +1,97 @@
+namespace Splitter.Framework.Tests
+{
+    using System;
+    using Splitter.Framework;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests for the DescriptionParser class.
+    /// </summary>
+    public class DescriptionParserTests
+    {
+        /// <summary>
+        /// Ensures when the passed description is null or empty, an argument exception is thrown.
+        /// </summary>
+        [Test]
+        [TestCase("")]
+        [TestCase((string)null)]
+        [TestCase("     ")]
+        public void ParseTracks_NullEmptyDescription_ArgumentException(string desc)
+        {
+            var service = this.GetInstance();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                service.ParseTracks(desc);
+            });
+        }
+
+        /// <summary>
+        /// Ensures when there are no tracks found, an invalid exception is thrown.
+        /// </summary>
+        [Test]
+        public void ParseTracks_NoTracksFound_InvalidOperationException()
+        {
+            var desc = "this is a description with no tracks";
+            var service = this.GetInstance();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                service.ParseTracks(desc);
+            });
+        }
+
+        /// <summary>
+        /// Ensures when there are tracks which are space delimited, they are parsed.
+        /// </summary>
+        [Test]
+        public void ParseTracks_TracksWithSpaceDelimited_Parsed()
+        {
+            var desc = "1.- 00:00 Salamander\n2.- 02:13 Mahou Hatsudou\n3.- 02:13 Dragon Force";
+            var service = this.GetInstance();
+
+            var tracks = service.ParseTracks(desc);
+
+            Assert.AreEqual(3, tracks.Count);
+
+            Assert.That(tracks.ContainsKey("Salamander"));
+            Assert.That(tracks.ContainsKey("Mahou Hatsudou"));
+            Assert.That(tracks.ContainsKey("Dragon Force"));
+
+            Assert.AreEqual("00:00", tracks["Salamander"]);
+            Assert.AreEqual("02:13", tracks["Mahou Hatsudou"]);
+            Assert.AreEqual("02:13", tracks["Dragon Force"]);
+        }
+
+        /// <summary>
+        /// Ensures when there are tracks which are dash delimited, they are parsed.
+        /// </summary>
+        [Test]
+        public void ParseTracks_TracksWithDashDelimited_Parsed()
+        {
+            var desc = "1.- 00:00-Salamander\n2.- 02:13-Mahou Hatsudou\n3.- 02:13-Dragon Force";
+            var service = this.GetInstance();
+
+            var tracks = service.ParseTracks(desc);
+
+            Assert.AreEqual(3, tracks.Count);
+
+            Assert.That(tracks.ContainsKey("Salamander"));
+            Assert.That(tracks.ContainsKey("Mahou Hatsudou"));
+            Assert.That(tracks.ContainsKey("Dragon Force"));
+
+            Assert.AreEqual("00:00", tracks["Salamander"]);
+            Assert.AreEqual("02:13", tracks["Mahou Hatsudou"]);
+            Assert.AreEqual("02:13", tracks["Dragon Force"]);
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <returns>the instance.</returns>
+        private DescriptionParser GetInstance()
+        {
+            return new DescriptionParser();
+        }
+    }
+}
