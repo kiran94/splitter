601ff38d959f24130faef3c3626000db24bb4639
Kiran94 <kiran_patel94@hotmail.com>
2018-04-21 16:20:58 +0100

    updated entry point

--- Splitter/Splitter.ConsoleApp/Program.cs
+++ Splitter/Splitter.ConsoleApp/Program.cs
@@ -11,9 +11,26 @@
             var url = "https://www.youtube.com/watch?v=ppzcjw2Xq1Y";
             var client = new YoutubeClient();
             var repository = new YoutubeRepository(client);
+            var descriptionParser = new DescriptionParser();
 
+            WriteLine("Getting Metadata");
             var description = repository.GetDescription(url);
-            Console.WriteLine(description);
+
+            WriteLine("Parsing Tracks");
+            var tracks = descriptionParser.ParseTracks(description);
+
+            WriteLine($"Found {tracks.Count} Tracks:");
+            foreach(var currentTrack in tracks)
+            {
+                Console.WriteLine(currentTrack.Key + " " + currentTrack.Value);
+            }
+        }
+
+        static void WriteLine(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
         }
     }
 }
