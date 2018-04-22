1de17e792c5e1c24c57aae4e0196043e78019647
Kiran94 <kiran_patel94@hotmail.com>
2018-04-22 14:17:36 +0100

    added file io service

---
+++ Splitter/Splitter.Framework/Contracts/IFileIoService.cs
@@ -0,0 +1,20 @@
+namespace Splitter.Framework
+{
+    using System.IO;
+
+    /// <summary>
+    /// Encapsulates File IO operations.
+    /// </summary>
+    public interface IFileIoService
+    {
+        /// <summary>
+        /// Opens a stream to the given File.
+        /// </summary>
+        /// <param name="filePath">path to file</param>
+        /// <param name="mode">mode of open</param>
+        /// <param name="access">access mode.</param>
+        /// <param name="share">defines how other processes can access the file.</param>
+        /// <returns>Stream to the file.</returns>
+        Stream Open(string filePath, FileMode mode, FileAccess access, FileShare share);
+    }
+}
---
+++ Splitter/Splitter.Framework/Services/FileIoService.cs
@@ -0,0 +1,14 @@
+namespace Splitter.Framework
+{
+    using System.IO;
+
+    /// <inheritdoc />
+    public class FileIoService : IFileIoService
+    {
+        /// <inheritdoc />
+        public Stream Open(string filePath, FileMode mode, FileAccess access, FileShare share)
+        {
+            return File.Open(filePath, mode, access, share);
+        }
+    }
+}
