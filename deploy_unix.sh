23b719fa206cb5ca4b418e97fc09afe1971842f4
Kiran94 <kiran_patel94@hotmail.com>
2018-04-30 04:37:10 +0100

    added deploy unix instructions for aliasing

--- deploy_unix.sh
+++ deploy_unix.sh
@@ -6,3 +6,7 @@ mkdir /usr/local/bin/splitter;
 
 echo "Copying Artifacts..";
 cp -r -v Splitter/Splitter.ConsoleApp/bin/Release/netcoreapp2.0/publish/ /usr/local/bin/splitter;
+
+echo "Deployed Succesfully..";
+echo "Add the following alias:";
+echo "alias splitter='dotnet /usr/local/bin/splitter/Splitter.ConsoleApp.dll'";
